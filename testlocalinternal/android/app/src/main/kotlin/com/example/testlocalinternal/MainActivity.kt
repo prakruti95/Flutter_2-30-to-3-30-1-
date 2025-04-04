42516a90907507856a765bdec3eb31b75dcb209a
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2025-04-04 19:09:49 +0530

    commit

---
+++ testlocalinternal/android/app/src/main/kotlin/com/example/testlocalinternal/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.testlocalinternal
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ testlocalintl3/android/app/src/main/kotlin/com/example/testlocalintl3/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.testlocalintl3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
